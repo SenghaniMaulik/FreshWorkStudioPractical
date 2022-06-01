de21d613fab6f282c6418983435ecbbdd5ae6f4f
Maulik Senghani <senghani.maulik@gmail.com>
2022-06-01 09:08:40 +0530

    Initial commit

--- app/src/main/java/com/demo/freshworkstudiopractical/common/AdapterClickListener.kt
+++ app/src/main/java/com/demo/freshworkstudiopractical/common/AdapterClickListener.kt
@@ -1,7 +1,7 @@
-package com.demo.freshworkstudiopractical.common;
+package com.demo.freshworkstudiopractical.common
 
-import android.view.View;
+import android.view.View
 
-public interface AdapterClickListener {
-    void onItemClick(View view, int pos, Object object);
+interface AdapterClickListener {
+    fun onItemClick(view: View?, pos: Int, `object`: Any?)
 }
